11cd202b8f757805afaa5235d370c0513fbd06f9
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-05-15 21:23:19 -0500

    bundling

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import path from "path";
+
+export default {
+  devtool: "inline-source-map",
+  entry: [
+    path.resolve(__dirname, "src/index")
+  ],
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath:"/",
+    filename: "bundle.js"
+  },
+  plugins: [],
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  }
+}
